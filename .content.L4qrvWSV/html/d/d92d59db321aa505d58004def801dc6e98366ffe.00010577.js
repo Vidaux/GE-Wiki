3697ca99c98bd3a453c8552a094db6f7e8f7417a
root <root@ge-wiki.lan>
2025-08-11 03:49:56 +0000

    Updated image links and fixed css

---
+++ .content.L4qrvWSV/html/d/d92d59db321aa505d58004def801dc6e98366ffe.00010577.js
@@ -0,0 +1,34 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function Page(){var b=document.getElementById("threadCounter"),d=document.getElementById("contentThreadList");if(b){b=1*b.title}if(d){WPC.Action.execute("expandToggle",{"action":"collapse","parentNode":d});WPC.Action.execute("expandToggle",{"item":1,"action":"expand","parentNode":d});var a=WPC.DOM.getElementsByClassName(d,"div","WPC-tagModule");for(var c=a.length-1;c>=0;c--){a[c].style.display="none"}}})();
+
+}
+/*
+     FILE ARCHIVED ON 02:30:36 Oct 08, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:14:48 Aug 11, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 8.606
+  exclusion.robots: 0.019
+  exclusion.robots.policy: 0.009
+  esindex: 0.012
+  cdx.remote: 14.896
+  LoadShardBlock: 111.451 (3)
+  PetaboxLoader3.datanode: 102.15 (5)
+  load_resource: 57.37 (2)
+*/
